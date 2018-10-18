62a322028335fc1858809217d72b4eec7b218ed8
Piotrekf3 <piotrek.fur@wp.pl>
2018-10-18 19:34:41 +0200

    [PMD] lab2 - in progress

--- pmd/lab2/main.py
+++ pmd/lab2/main.py
@@ -1,11 +1,13 @@
 from random import *
 import itertools
 from numpy.random import choice
+import numpy as np
+import matplotlib.pyplot as plt
 
 def find_pairs(lst):
     return [(a,b) for a,b in itertools.permutations(lst, 2) if a==b]
 
-n = 10
+n = 1000
 p = 0.1
 nh = 100
 nd = 100
@@ -13,12 +15,47 @@ data = [[0 for x in range(n)] for y in range(nd)]
 terrorists = [[0 for x in range(n)] for y in range(n)] 
 seed()
 
+# for i in range(4):
+#     for j in range(i):
+#         print(i, j)
+
+
 for day in range(nd):
     for man in range(n):
         if choice([True, False], p = [p, 1-p]):
             data[day][man] = randint(1, nh)
+
+count = 0
 for day in range(nd):
     for man in range(n):
-        for man2 in range(n):
-            if data[day][man] == data[day][man2]
-                terrorists[day][man] += 1
+        for man2 in range(man):
+            if data[day][man] != 0 and man != man2 and data[day][man] == data[day][man2]:
+                terrorists[man][man2] += 1
+                if(terrorists[man][man2] == 2):
+                    count+=1
+# # print(terrorists)
+print(count)
+values, counts = np.unique(terrorists, return_counts=True)
+print(values, counts)
+plt.bar(values, counts, 0.5)
+plt.xticks(values)
+plt.show()
+
+
+
+
+# count = 0
+# for day in range(nd):
+#     for man in range(n):
+#         if choice([True, False], p = [p, 1-p]):
+#             data[day][man] = randint(1, nh)
+#         for man2 in range(man):
+#             if data[day][man] != 0 and man != man2 and data[day][man] == data[day][man2]:
+#                 terrorists[man][man2] += 1
+#                 if(terrorists[man][man2] == 2):
+#                     count+=1
+
+# print(terrorists)
+# print(count)
+# plt.hist(terrorists)
+# plt.show()
