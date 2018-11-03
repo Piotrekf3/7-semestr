af1be94bd9bec6a59135386f07b31c01e146f6b1
Piotrekf3 <piotrek.fur@wp.pl>
2018-11-03 18:11:18 +0100

    results

--- TPSZ/Tasks scheduling/src/Main.java
+++ TPSZ/Tasks scheduling/src/Main.java
@@ -1,12 +1,18 @@
 public class Main {
     public static void main(String[] args) {
         String instancesPath = "instances/";
-        double h = 0.8;
-        Instance[] instances = new InstancesLoader(instancesPath + "sch1000.txt").load();
+        double h = 0.2;
+        Instance[] instances = new InstancesLoader(instancesPath + "sch500.txt").load();
+        long startTime;
+        long timeElapsed;
         for(int i=0; i<instances.length; i++) {
+            startTime = System.nanoTime();
             instances[i].schedule(h);
+            timeElapsed = System.nanoTime() - startTime;
+            System.out.println(timeElapsed / 1000000.0);
             instances[i].saveToFile(i, h);
         }
+
 //        if(Tester.test(instancesPath + "sch10.txt", "results/n10k3h2.txt"))
 //            System.out.println("Ok");
 //        else
