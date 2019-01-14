2e4964a5f1e91255e06426f6f5ec1d3f3d3b4e07
Piotrekf3 <piotrek.fur@wp.pl>
2019-01-14 19:41:42 +0100

    [PMD] Add minhash-jaccard comparision'

--- pmd/minhash/src/main.py
+++ pmd/minhash/src/main.py
@@ -15,8 +15,8 @@ def loadInput():
             if user not in dict:
                 dict[user] = []
             dict[user].append(song)
-    for user in dict.keys():
-        dict[user].sort()
+    # for user in dict.keys():
+    #     dict[user].sort()
     return dict
 
 def hash(value):
@@ -26,10 +26,40 @@ def newSignature(users):
     global rand
     signature = list()
     rand = random.randint(0, signatures_number)
-    for user, songs in users.items():
-       signature.append(min(map(hash, songs)))
+    for i in range(1, users_number + 1):
+        signature.append(min(map(hash, users[i])))
     return signature
 
+def jaccardSimilarity(list1, list2):
+    intersection = len(list(set(list1).intersection(list2)))
+    union = (len(list1) + len(list2)) - intersection
+    return float(intersection / union)
+
+def getRealJaccard(users):
+    similarity = {}
+    for i in range(1, users_number + 1):
+        for j in range(1, users_number + 1):
+            similarity[(i, j)] = jaccardSimilarity(users[i], users[j])
+    return similarity
+
+def getEstimatedJaccard(signatures):
+    similarity = {}
+    for signature in signatures:
+        for i in range(1, users_number + 1):
+            for j in range(1, users_number + 1):
+                if signature[i - 1] == signature[j - 1]:
+                    similarity[(i, j)] = similarity.get((i, j), 0) + 1
+    for key in similarity:
+        similarity[key] /= signatures_number
+    return similarity
+
+def getRMSE(realValues, estimatedValues):
+    result = 0
+    for key in estimatedValues:
+        result += (realValues[key] - estimatedValues[key])**2
+    result /= len(estimatedValues)
+    print(len(estimatedValues))
+    return result
 
 random.seed()
 users = loadInput()
@@ -38,3 +68,12 @@ signatures = list()
 for i in range(signatures_number):
     signatures.append(newSignature(users))
     print(i)
+
+estimatedJaccard = getEstimatedJaccard(signatures)
+# print(estimatedJaccard)
+realJaccard = getRealJaccard(users)
+# print(realJaccard)
+print(getRMSE(realJaccard, estimatedJaccard))
+
+
+
